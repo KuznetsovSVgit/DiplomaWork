2cb578f3bf2dd39fb16fc454b573229f0ab30137
unknown <79102451200@yandex.ru>
2019-03-21 16:42:10 +0300

    add slider-info

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,30 @@ $(document).ready(function(){
 var btnmenu = $('#btnmenu');
 var modalMenu = $('#modalMenu');
 var modalMenuClose = $('#modalMenuClose');
+var slick = $('#slick');
 
+$('.slick').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  prevArrow: $('.slick__leftArrow'),
+  nextArrow: $('.slick__rightArrow'),
+});
+// Слайдер
+// $(window).resize(function() {
+// if ($(window).width() < 576) {
+//  slick.removeClass('info-block');
+//  slick.addClass('slick');
+
+// })
+
+// Открытие/закрытие меню
 btnmenu.on('click', function(){
 modalMenu.addClass('modalMenu_active');
-  });
-
+  })
 modalMenuClose.on('click', function(){
 modalMenu.removeClass('modalMenu_active');
-})
 });
+});
+
